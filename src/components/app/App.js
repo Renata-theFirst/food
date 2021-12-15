272578e509f74487d585a8b6839b598173e068c1
Renata-theFirst <kisulja170691@gmail.com>
2021-12-15 21:15:35 +0300

    some correction

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,23 +5,26 @@ import SidePanel from '../sidePanel/SidePanel.js';
 import Calc from '../calc/Calc.js';
 import Footer from '../footer/Footer.js';
 import ModalDialog from '../modal/ModalDialog.js';
+import Promotion from '../promotion/Promotion.js';
 
 
 const App = () => {
   return(
    
      <>
-      <AppHeader/>
-      <SidePanel/>
-      <Preview/>
-      <div className="divider"></div>
-      <MainInfo/>
-      <div className="divider"></div>
-      <Calc/>
-      <Footer/>
-      <ModalDialog/>
+        <AppHeader/>
+        <SidePanel/>
+        <Preview/>
+        <div className="divider"></div>
+        <MainInfo/>
+        <div className="divider"></div>
+        <Calc/>
+        <div className="divider"></div>
+        <Promotion/>
+        <Footer/>
+        <ModalDialog/>
       </>
-  )
-}
+  );
+};
 
 export default App;
